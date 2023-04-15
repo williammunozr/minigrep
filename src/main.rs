b9f46235b858d1d04241f36c299a7721aa20342b
william <william.munozr@gmail.com>
2023-04-15 09:19:23 -0500

    implementing case sensitive and insensitive searchs

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,12 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        println!("Result error: {}", err);
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
-
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        println!("Result error: {}", e);
         process::exit(1);
     }
 }
